64dfabb332c1d15b1e0d49a11d509a5d0c3f3aa8
Rabiul-Hossen <rhsimon69@gmail.com>
2022-12-30 00:50:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { Route, Routes } from "react-router-dom";
+import { io } from "socket.io-client";
+
 import './App.css';
+import Login from "./Pages/Login";
+import MyPhotos from "./Pages/MyPhotos";
+import Photos from "./Pages/Photos";
+import Register from "./Pages/Register";
+
+
+//👇🏻 http://localhost:4000 is where the server host URL.
 
 function App() {
+  const socket = io.connect("http://localhost:5000");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<Routes>
+<Route path="/photos" element={<Photos socket={socket}/>}/>
+<Route path="/myphotos" element={<MyPhotos socket={socket}/>}/>
+<Route path="/" element={<Login socket={socket} />}/>
+<Route path="/register" element={<Register socket={socket}/>}/>
+
+</Routes>
+    </>
   );
 }
 
